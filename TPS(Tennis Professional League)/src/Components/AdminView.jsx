14e1c00d85c425307f9d4752389df90e8b5c299b
YinelaPrimero <yinelaprimero01@gmail.com>
2024-05-20 23:32:00 -0500

    Avances CRUD Administrador

--- TPS(Tennis Professional League)/src/Components/AdminView.jsx
+++ TPS(Tennis Professional League)/src/Components/AdminView.jsx
@@ -1,8 +1,86 @@
 import React from 'react'
+import { createTourney, deleteTourney, readTourney, readTourneyById, updateTourney} from '../Firebase/crud';
 
 function AdminView() {
   return (
-    <div>Hola, admin</div>
+
+    <div>Hola, admin
+      
+      <input type="text" id="name-tourney" placeholder="Nombre del torneo" />
+      <input type="date" id="deadline-tourney" />
+      {/*<input type="file" id="image-tourney" />*/}
+      <input type="number" id="limit-tourney" placeholder="Límite de participantes" />
+      
+      <button
+        onClick={async () => {
+          const input1 = document.getElementById('name-tourney').value;
+          const input2 = document.getElementById('deadline-tourney').value;
+          // const input3 = document.getElementById('image-tourney').files[0]; // Descomentar si se utiliza la imagen
+          const input4 = parseInt(document.getElementById('limit-tourney').value, 10);
+
+          if (input1 && input2 && input4) {
+            await createTourney(input1, input2, input4);
+            console.log('The tourney was created');
+          } else {
+            console.error('Por favor, completa todos los campos requeridos.');
+          }
+        }}
+      >
+        Crear Torneo
+      </button>
+
+      <button
+        onClick={async () => { 
+          const response = await readTourney();
+          console.log("response ", response);
+        }}
+      >
+        Ver torneos
+      </button>
+
+      {/*<button
+        onClick={async () => { 
+          const id = "qnLbKmCNaXmi60lvYCR2";
+          const response = await readTourneyById(id);
+          console.log("response ", response);
+        }}
+      >
+        Ver torneo específico
+      </button>
+      */}
+
+      <button
+        onClick={async () => {
+          const id = "qnLbKmCNaXmi60lvYCR2"; //Organizar para que el id pueda suministrarlo en administrador
+          const input1 = document.getElementById('name-tourney').value;
+          const input2 = document.getElementById('deadline-tourney').value;
+          // const input3 = document.getElementById('image-tourney').files[0]; // Descomentar si se utiliza la imagen
+          const input4 = parseInt(document.getElementById('limit-tourney').value, 10);
+
+          if (id && input1 && input2 && !isNaN(input4)) {
+            await updateTourney(id, input1, input2, input4);
+            console.log('The tourney was updated successfully');
+          } else {
+            console.error('Por favor, completa todos los campos requeridos.');
+          }
+        }}
+      >
+        Actualizar Torneo
+      </button>
+
+      <button
+        onClick={async () => { 
+          const id = "qnLbKmCNaXmi60lvYCR2";
+          const response = await deleteTourney(id);
+          console.log("Tourney deleted response:", response);
+        }}
+      >
+        Eliminar torneo específico
+      </button>
+
+
+    </div>
+    
   )
 }
 
---
+++ TPS(Tennis Professional League)/src/Firebase/crud.js
@@ -0,0 +1,77 @@
+import {
+  addDoc,
+  doc,
+  collection,
+  getDocs,
+  where,
+  documentId,
+  query,
+  getDoc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "./credenciales";
+
+async function createTourney(name, registrationDeadline, limitNumberParticipants) { //Falta implementar y pensar lo de la imagen :c
+    try {
+      const docRef = await addDoc(collection(db, "tourney"), {
+        name: name,
+        registrationDeadline: registrationDeadline,
+        limitNumberParticipants: limitNumberParticipants,
+      });
+  
+      console.log("The tourney was created: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding the tourney: ", e);
+    }
+  }
+
+async function readTourney() {
+    try {
+      const querySnapshot = await getDocs(collection(db, "tourney"));
+      const response = querySnapshot.docs.map((doc) => doc.data());
+      return response;
+    } catch (e) {
+      console.error("Error reading the tourneys: ", e);
+      return [];
+    }
+  }
+
+//Está generando problema por el getDocument
+  async function readTourneyById(id) {
+    const querySnapshot = await getDocument("tourney", id);
+    console.log("querySnapshot", querySnapshot);
+    return null;
+  }
+
+async function updateTourney(id, name, registrationDeadline, limitNumberParticipants ) {
+  try {
+    
+    const userRef = doc(db, "tourney", id);
+
+    await updateDoc(userRef, { 
+        name: name,
+        registrationDeadline: registrationDeadline,
+        limitNumberParticipants: limitNumberParticipants });
+
+    console.log("Document successfully updated");
+
+  } catch (e) {
+    console.error("Error updating document: ", e);
+  }
+
+}
+
+async function deleteTourney(id) {
+  try {
+    const userRef = doc(db, "tourney", id);
+    await deleteDoc(userRef);
+    console.log("Tourney deleted");
+
+  } catch (e) {
+    console.error("Error deleting tourney ", e);
+  }
+}
+
+
+export {createTourney,readTourney,readTourneyById,updateTourney,deleteTourney};
